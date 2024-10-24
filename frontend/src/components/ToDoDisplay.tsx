6f69c2a490bf8c28781b164c975d900865283740
Chris Daly <chrisdalyenq@gmail.com>
2024-10-23 19:25:55 -0700

    Create a column for each status

--- frontend/src/components/ToDoDisplay.tsx
+++ frontend/src/components/ToDoDisplay.tsx
@@ -1,11 +1,12 @@
 import Stack from "@mui/material/Stack/Stack";
 import {StatusColumn} from "./StatusColumn.tsx";
+import { taskStatuses } from "../api/tasks.ts";
 
 const ToDoDisplay = () => (
     <Stack direction="row" spacing={5} paddingInline={5}>
-        <StatusColumn status="To Do"/>
-        <StatusColumn status="In Progress"/>
-        <StatusColumn status="Done"/>
+        {taskStatuses.map(s => (
+            <StatusColumn status={s} key={s}/>
+        ))}
     </Stack>
 )
 
