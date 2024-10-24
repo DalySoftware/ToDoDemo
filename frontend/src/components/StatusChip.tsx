80ababa0b499599c5c198a18ee74181a669f8773
Chris Daly <chrisdalyenq@gmail.com>
2024-10-24 00:10:58 -0700

    Prettier format

--- frontend/src/components/StatusChip.tsx
+++ frontend/src/components/StatusChip.tsx
@@ -1,17 +1,15 @@
 import { ReactNode } from "react";
 import { TaskStatus } from "../api/tasks";
-import ListIcon from '@mui/icons-material/List';
+import ListIcon from "@mui/icons-material/List";
 import CheckCircle from "@mui/icons-material/CheckCircle";
-import PendingIcon from '@mui/icons-material/Pending';
+import PendingIcon from "@mui/icons-material/Pending";
 
-const mapping: Record<TaskStatus, ReactNode> =  {
-    "To Do": <ListIcon fontSize="large" color="primary"/>,
-    "In Progress": <PendingIcon  fontSize="large" color="warning"/>,
-    "Done": <CheckCircle  fontSize="large" color="success"/>,
-}
+const mapping: Record<TaskStatus, ReactNode> = {
+  "To Do": <ListIcon fontSize="large" color="primary" />,
+  "In Progress": <PendingIcon fontSize="large" color="warning" />,
+  Done: <CheckCircle fontSize="large" color="success" />,
+};
 
-const StatusIcon = ({status}: { status: TaskStatus}) => (
-    mapping[status]
-)
+const StatusIcon = ({ status }: { status: TaskStatus }) => mapping[status];
 
-export { StatusIcon }
+export { StatusIcon };
